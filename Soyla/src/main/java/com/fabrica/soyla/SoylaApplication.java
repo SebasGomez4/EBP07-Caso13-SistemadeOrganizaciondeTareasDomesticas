d77e2b79eb43c51a8dec44a4c86db3436a2b94a5
MiguelAvenda07 <m.avendano@udea.edu.co>
2026-03-24 15:32:22 -0500

    Inicialización del proyecto

---
+++ Soyla/src/main/java/com/fabrica/soyla/SoylaApplication.java
@@ -0,0 +1,13 @@
+package com.fabrica.soyla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoylaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoylaApplication.class, args);
+	}
+
+}
---
+++ Soyla/src/test/java/com/fabrica/soyla/SoylaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrica.soyla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoylaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

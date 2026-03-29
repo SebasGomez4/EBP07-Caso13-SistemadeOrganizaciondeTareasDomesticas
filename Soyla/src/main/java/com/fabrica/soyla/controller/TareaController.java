502690b6cff46dad08f18dd07f4ec7b40cce9bb2
Sam-eb <samuel.tabaresg@udea.edu.co>
2026-03-28 20:35:44 -0500

    Exception Handler para toda la app
    Se agrego el control de errores para toda la app, para que el error devuelto en la api sea más claro

--- Soyla/src/main/java/com/fabrica/soyla/controller/TareaController.java
+++ Soyla/src/main/java/com/fabrica/soyla/controller/TareaController.java
@@ -15,9 +15,13 @@ public class TareaController {
     @Autowired
     private TareaService tareaService;
 
-    @PostMapping
-    public ResponseEntity<TareaDomestica> crearTarea(@Valid @RequestBody TareaDomestica tarea) {
-        TareaDomestica nueva = tareaService.crearTarea(tarea);
-        return ResponseEntity.status(HttpStatus.CREATED).body(nueva);
+     @PostMapping
+    public ResponseEntity<?> crearTarea(@Valid @RequestBody TareaDomestica tarea) {
+        try {
+            TareaDomestica nueva = tareaService.crearTarea(tarea);
+            return ResponseEntity.status(HttpStatus.CREATED).body(nueva);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 }
